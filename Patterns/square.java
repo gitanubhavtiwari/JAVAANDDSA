609aaca123d75019abae4773bd62aac3b2cc2b8f
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2025-02-03 18:31:15 +0530

    Update square.java

--- Patterns/square.java
+++ Patterns/square.java
@@ -1,12 +1,12 @@
-public class square { // Class name starts with uppercase
-    public static void main(String[] args) { // Added main method
-        int size = 4; // Size of the square
+public class square { 
+    public static void main(String[] args) { 
+        int size = 4; 
 
-        for (int i = 1; i <= size; i++) { // Outer loop for rows
-            for (int j = 1; j <= size; j++) { // Inner loop for columns
+        for (int i = 1; i <= size; i++) { 
+            for (int j = 1; j <= size; j++) { 
                 System.out.print("* ");
             }
-            System.out.println(); // Move to the next line after each row
+            System.out.println(); 
         }
     }
 }
