614e2fc2f1dc6a115f09526379b26c9308a19ed1
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2025-02-10 01:59:52 +0530

    Added FUNCTIONS folder

---
+++ FUNCTIONS/parameters.java
@@ -0,0 +1,15 @@
+package JAVAANDDSA.FUNCTIONS; // Add this at the top
+import java.util.*;
+public class parameters {
+    public static int calculatesum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = calculatesum(a, b);
+    System.out.println("Sum is: " + sum);
+ }
+}
