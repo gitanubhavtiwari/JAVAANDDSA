e56b424d91bc15d4760bea8b1a874adc3ad806a0
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2025-02-10 19:31:54 +0530

    Update parameters.java

--- FUNCTIONS/parameters.java
+++ FUNCTIONS/parameters.java
@@ -1,7 +1,7 @@
 package JAVAANDDSA.FUNCTIONS; // Add this at the top
 import java.util.*;
 public class parameters {
-    public static int calculatesum(int a, int b) {
+    public static int calculatesum(int a, int b) { //parameters or formal parameters
         int sum = a + b;
         return sum;
     }
@@ -9,7 +9,7 @@ public class parameters {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
     int b = sc.nextInt();
-    int sum = calculatesum(a, b);
+    int sum = calculatesum(a, b); // arguments or actual parameters
     System.out.println("Sum is: " + sum);
  }
 }
