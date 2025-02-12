be03f30e728676abe8d2df4d224bc6e3967388dc
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2025-02-12 14:05:07 +0530

    PRIME

---
+++ FUNCTIONS/PRIME.java
@@ -0,0 +1,28 @@
+package JAVAANDDSA.FUNCTIONS;
+
+import java.util.*;
+
+public class PRIME {
+
+    public static boolean PRIME(int n) {
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(PRIME(n));
+    }
+
+}
