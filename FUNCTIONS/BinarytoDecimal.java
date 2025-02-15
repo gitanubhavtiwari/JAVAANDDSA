879e0cbc1fa68676498c4b83a30ace7e50e5d7f7
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2025-02-15 11:16:29 +0530

    BinarytoDecimal

--- FUNCTIONS/BinarytoDecimal.java
+++ FUNCTIONS/BinarytoDecimal.java
@@ -1,15 +1,22 @@
 package JAVAANDDSA.FUNCTIONS;
 
 public class BinarytoDecimal {
-    public static void main(String[] args) {
-        int n = 111001; 
+
+    public static int BINTODECI(int n) {
         int t = n ,d = 0;
         for(int i=0; t>0;i++ ){
             int r = t%10;
             t=t/10;
             d= d + r* (int)Math.pow(2,i);
         }
+        return d;
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+       int d = BINTODECI(1111);
         System.out.println(d);
     }
-  
 }
