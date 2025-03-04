a83828131363b3ceb0ea65a7aa36870803350e9a
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2025-03-04 12:35:21 +0530

    AVERAGE

---
+++ FUNCTIONS/AVERAGE.java
@@ -0,0 +1,16 @@
+package JAVAANDDSA.FUNCTIONS;
+import java.util.Scanner;
+public class AVERAGE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter First Number");
+        int a = sc.nextInt();
+        System.err.println("Enter Second Number");
+        int b = sc.nextInt();
+        System.err.println("Enter third number");
+        int c = sc.nextInt();
+        int Average = (a+b+c)/3;
+        System.err.println("Average is"+Average);
+        sc.close();
+    }
+}
